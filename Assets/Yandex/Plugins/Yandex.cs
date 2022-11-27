fe1743cf1ca4cd6218a48ebffdd4f904e0839dca
dimmmark <105634628+dimmmark@users.noreply.github.com>
2022-11-27 12:26:44 +0400

    Add yandex sdk

---
+++ Assets/Yandex/Plugins/Yandex.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Yandex : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+    [DllImport("__Internal")]
+    private static extern void GiveMePlayerData();
+
+    [DllImport("__Internal")]
+    private static extern void RateGame();
+
+
+    public void RateGameButton()
+    {
+        RateGame();
+    }
+
+    public void HelloButton()
+    {
+        GiveMePlayerData();
+    }
+
+    [SerializeField] TextMeshProUGUI _nameText;
+    [SerializeField] RawImage _photo;
+
+    public void SetName(string name)
+    {
+        _nameText.text = name;
+    }
+    public void SetPhoto(string url)
+    {
+        StartCoroutine(DownloadImage(url));
+    }
+    IEnumerator DownloadImage(string madiaUrl)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(madiaUrl);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            Debug.Log(request.error);
+        else
+            _photo.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+    }
+}
